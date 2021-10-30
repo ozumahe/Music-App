8c6d56a69adaac489c0d01f0255a02b7f115f97e
ozumahe <ozumahe@gmail.com>
2021-10-30 18:01:49 +0100

    first commit

--- script.js
+++ script.js
@@ -7,83 +7,6 @@ const musics = [
     musicSrc:
       "https://9jaflaver.com/wp-content/uploads/2021/07/Phyno_Bia_9jaflaver.com_.mp3",
     musicAuthor: "Phyno",
-    MusicLyricsText: `
-    Master
-    Amara Chineke emewom' mma 
-    Áká Chineke asawom' njọ 
-    Ọbara Chineke emewom' mma o
-    Omeokachie
-    Chineke emewom' mma
-    Amara Chineke emewom' mma
-    Áká Chineke asawom' njọ
-    Ọbara Chineke emewom' mma o
-    Omeokachie
-    Chineke emewom' mma o
-    Igirigi nké ndị nsọ
-    Bịa ọ, bịa ọ, bịa ọ
-    Mepee ọ n'uzo enụ ìgwè
-    Bịa ọ, bịa ọ, bịa ọ
-    Obi n'ígwé anyị ná ayo nụ gị
-    Bịa ọ, bịa ọ, bịa o
-    Lele
-    Bịa ọ, bịa ọ, bịa ọ
-    Febe lu Chineke áká ọ
-    Tobe yá ná ọbụ Ezé ọ
-    'Cause he don reason my matter o
-    Wè gaziere lum' ana ezé o
-    Even when I no get money
-    Guaranteed that I get somebody
-    Everyday you dey watch over me
-    Send your blessings to come follow me
-    Ụwa cho ịgba jim áká o
-    Only you be the answer o
-    Give me signs, give me wonder o
-    I go serve you my lifetime
-    'Cause your love is enough, màkà Chi
-    And ọgba jụọ
-    Na you dey cover me
-    All days of my life
-    Igirigi nké ndị nsọ
-    Bịa ọ, bịa ọ, bịa ọ
-    Mepee ọ n'uzo enụ ìgwè
-    Bịa ọ, bịa ọ, bịa ọ
-    Obi n'ígwé anyị ná ayo nụ gị
-    Bịa ọ, bịa ọ, bịa o
-    Lele
-    Bịa ọ, bịa ọ, bịa ọ
-    You shine your light
-    And bless my life
-    Wè sa mụ njọ
-    Now I'm brand new
-    You uplift my soul
-    You give me joy wey nobody fit to give me
-    Papa o
-    Ọnụ m' jụrụ ekene o
-    You shine your light
-    And bless my life
-    Wè sa mụ njọ
-    Now I'm brand new
-    You uplift my soul
-    You give me joy wey nobody fit to give me
-    Papa o
-    Ọnụ m' jụrụ ekene
-    Igirigi nké ndị nsọ
-    Bịa ọ, bịa ọ, bịa ọ
-    Mepee ọ n'uzo enụ ìgwè
-    Bịa ọ, bịa ọ, bịa ọ
-    Obi n'ígwé anyị ná ayo nụ gị
-    Bịa ọ, bịa ọ, bịa o
-    Lele
-    Bịa ọ, bịa ọ, bịa ọ
-    Igirigi nké ndị nsọ
-    Bịa ọ, bịa ọ, bịa ọ
-    Mepee ọ n'uzo enụ ìgwè
-    Bịa ọ, bịa ọ, bịa ọ
-    Obi n'ígwé anyị ná ayo nụ gị
-    Bịa ọ, bịa ọ, bịa o
-    Lele
-    Bịa ọ, bịa ọ, bịa ọ
-    Extraordinaire`,
   },
   {
     id: 2,
@@ -93,83 +16,6 @@ const musics = [
     musicSrc:
       "https://9jaflaver.com/wp-content/uploads/2021/09/Phyno_Stacks_9jaflaver.com_.mp3",
     musicAuthor: "Phyno",
-    MusicLyricsText: `
-    Master
-    Amara Chineke emewom' mma 
-    Áká Chineke asawom' njọ 
-    Ọbara Chineke emewom' mma o
-    Omeokachie
-    Chineke emewom' mma
-    Amara Chineke emewom' mma
-    Áká Chineke asawom' njọ
-    Ọbara Chineke emewom' mma o
-    Omeokachie
-    Chineke emewom' mma o
-    Igirigi nké ndị nsọ
-    Bịa ọ, bịa ọ, bịa ọ
-    Mepee ọ n'uzo enụ ìgwè
-    Bịa ọ, bịa ọ, bịa ọ
-    Obi n'ígwé anyị ná ayo nụ gị
-    Bịa ọ, bịa ọ, bịa o
-    Lele
-    Bịa ọ, bịa ọ, bịa ọ
-    Febe lu Chineke áká ọ
-    Tobe yá ná ọbụ Ezé ọ
-    'Cause he don reason my matter o
-    Wè gaziere lum' ana ezé o
-    Even when I no get money
-    Guaranteed that I get somebody
-    Everyday you dey watch over me
-    Send your blessings to come follow me
-    Ụwa cho ịgba jim áká o
-    Only you be the answer o
-    Give me signs, give me wonder o
-    I go serve you my lifetime
-    'Cause your love is enough, màkà Chi
-    And ọgba jụọ
-    Na you dey cover me
-    All days of my life
-    Igirigi nké ndị nsọ
-    Bịa ọ, bịa ọ, bịa ọ
-    Mepee ọ n'uzo enụ ìgwè
-    Bịa ọ, bịa ọ, bịa ọ
-    Obi n'ígwé anyị ná ayo nụ gị
-    Bịa ọ, bịa ọ, bịa o
-    Lele
-    Bịa ọ, bịa ọ, bịa ọ
-    You shine your light
-    And bless my life
-    Wè sa mụ njọ
-    Now I'm brand new
-    You uplift my soul
-    You give me joy wey nobody fit to give me
-    Papa o
-    Ọnụ m' jụrụ ekene o
-    You shine your light
-    And bless my life
-    Wè sa mụ njọ
-    Now I'm brand new
-    You uplift my soul
-    You give me joy wey nobody fit to give me
-    Papa o
-    Ọnụ m' jụrụ ekene
-    Igirigi nké ndị nsọ
-    Bịa ọ, bịa ọ, bịa ọ
-    Mepee ọ n'uzo enụ ìgwè
-    Bịa ọ, bịa ọ, bịa ọ
-    Obi n'ígwé anyị ná ayo nụ gị
-    Bịa ọ, bịa ọ, bịa o
-    Lele
-    Bịa ọ, bịa ọ, bịa ọ
-    Igirigi nké ndị nsọ
-    Bịa ọ, bịa ọ, bịa ọ
-    Mepee ọ n'uzo enụ ìgwè
-    Bịa ọ, bịa ọ, bịa ọ
-    Obi n'ígwé anyị ná ayo nụ gị
-    Bịa ọ, bịa ọ, bịa o
-    Lele
-    Bịa ọ, bịa ọ, bịa ọ
-    Extraordinaire`,
   },
   {
     id: 3,
@@ -179,104 +25,6 @@ const musics = [
     musicSrc:
       "https://9jaflaver.com/wp-content/uploads/2021/04/King_Perryy_Ft_Phyno_Paulina_9jaflaver.com_.mp3",
     musicAuthor: "King_Perryy_Ft_Phyno",
-    MusicLyricsText: `<p>
-    Mmm-hmmm
-    <br/>
-    Continental
-    <br/>
-    <br/>
-
-    You blow my mind girl
-    Can I come and get ya
-    Don't be shy girl
-    You gon' like things I'ma get ya
-    Blow my mind girl
-    Can I come and get ya
-    Don't be shy girl (yeah, eh)
-    Clear your container (ogbe)
-    Crystal clear like an antenna (eh)
-    When I come through with my man dema
-    Rub the surface with your bandanna eh
-    Clear your container
-    Crystal clear like an antenna
-    When I come through with my man dema
-    Rub the surface with your bandanna eh
-    Nwa oso muna gi now
-    Imelu Obi ana atom Paulina Paulina
-    And so gi mamu ofuma ka afa gi abu Sabina
-    Gwam ife iga eli for breakfast launch Na dinner
-    Mga ye gi isi Nwoke nwelu stamina
-    Kama e ga eli gi munwa eje cholu gi katika
-    My baby malu nma Na agba ka shakira
-    Agam agalu gi nga ka mu abu madiba
-    Ibu my queen and adim ekwu latifa
-    Nwa I go love you till nme your friends Believers
-    Juo egberi juo Perry juo mufasa
-    Etu nsi ebi ma anyi kponyesienu cubana
-    Gimme your true colors ka nwunyelugi paint
-    Nma tiwa Obi gi nga alunyelugi gate
-    And inwe Nsogbu ikpom ka nkolu gi case
-    I no go fall your hand I no go keep you waiting
-    Ibulu my date
-    Blow my mind girl
-    (Paulina)
-    Can I come and get ya
-    (Paulina, Paulina)
-    Don't be shy girl
-    (Paulina)
-    You gon' like things l'ma get ya
-    (Paulina, Paulina)
-    Blow my mind girl
-    (Paulina)
-    Can I come and get ya
-    (Paulina, Paulina)
-    Don't be shy girl
-    Clear your container
-    Crystal clear like an antenna
-    When I come through with my man dema
-    Rub the surface with your bandanna yeah
-    Clear your container
-    Crystal clear like an antenna
-    When I come through with my man dema
-    Rub the surface with your bandanna yeah
-    My girl you fine like Anita Baker
-    You too fine with no make up
-    Body shape toh bad wake up
-    Can I touch down then we take off (take off)
-    See the way you whine my girl
-    You know that you are picture perfect uh-uh
-    The way you move my girl
-    Your backa fit make man crazeee
-    Blow my mind girl
-    (Paulina)
-    Can I come and get ya
-    (Paulina, Paulina)
-    Don't be shy girl
-    (Paulina)
-    You gon' like things l'ma get ya
-    (Paulina, Paulina)
-    Blow my mind girl
-    (Paulina)
-    Can I come and get ya
-    (Paulina, Paulina)
-    Don't be shy girl
-    Clear your container
-    Crystal clear like an antenna
-    When I come through with my man dema
-    Rub the surface with your bandanna yeah
-    Clear your container
-    Crystal clear like an antenna
-    When I come through with my man dema
-    Rub the surface with your bandanna yeah
-    Paulina (oh baby)
-    Paulina Paulina (baby)
-    Paulina (oh baby)
-    Paulina Paulina (baby baby)
-    Paulina (oh baby)
-    Paulina Paulina (baby)
-    Paulina (baby)
-    Paulina Paulina
-    </p>`,
   },
 ];
 
@@ -290,105 +38,8 @@ function _sel(sel) {
 }
 
 // id getters
-// const playerContainer = _id("player-container");
-const container = _id("container");
-// const showLyrics = _id("show-lyrics");
-// const shuffle = _id("shuffle");
-// const prev = _id("prev");
-// const play = _id("play");
-// const next = _id("next");
-// const repeat = _id("repeat");
-// const favorite = _id("favorite");
-// const moreMusic = _id("more-music");
-// const audio = _id("audio");
-// const lyricsText = _id("lyrics-text");
-// const title = _id("title");
-// const artist = _id("artist");
-// const image = _id("image");
-
-// const show = _sel(".show-lyrics .fas");
-// const lyricsContainer = _sel(".lyrics-container");
-// const playBtn = _sel(".play .fas");
-
-musics.forEach((music) => {
-  const listContainer = document.createElement("div");
-  listContainer.innerHTML = `
-    <div class="list">
-       <h4>${music.musicTitle}</h4>
-       <small>${music.musicAuthor}</small>
-       <img src=${music.musicImageSrc} alt="" />
-     </div>
-   </div>
-    `;
-  container.append(listContainer);
-  console.log(music);
-
-  listContainer.addEventListener("click", () => {
-    console.log(music);
-    loadplaying(music);
-  });
-});
-
-function loadplaying(music) {
-  container.innerHTML = "";
-  const playingContainer = document.createElement("div");
-  playingContainer.innerHTML = `
-  <div id="player-container" class="player-container">
-        <div class="image">
-          <img id="image" src=${music.musicImageSrc} alt="" />
-        </div>
-        <div class="about">
-          <div>
-            <h3 id="title" class="title">${music.musicTitle}</h3>
-            <h6 id="artist" class="artist">${music.musicAuthor}</h6>
-          </div>
-          <div>
-            <button id="favorite" class="favorite">
-              <i class="far fa-heart"></i>
-            </button>
-            <button id="more-music" class="more-music">
-              <i class="fas fa-stream"></i>
-            </button>
-          </div>
-        </div>
-        <div class="music-track">
-          <div class="track"></div>
-        </div>
-        <div class="music-counter">
-          <small>10:3</small>
-          <small>0:3</small>
-        </div>
-        <div class="control-btns">
-          <button id="shuffle" class="shuffle">
-            <i class="fas fa-random"></i>
-          </button>
-          <button id="prev" class="backward">
-            <i class="fas fa-backward"></i>
-          </button>
-          <button id="play" class="play active">
-            <i class="fas fa-play"></i>
-          </button>
-          <button id="next" class="forward">
-            <i class="fas fa-forward"></i>
-          </button>
-          <button id="repeat" class="repeat">
-            <i class="fas fa-repeat"></i>
-          </button>
-        </div>
-        <div class="lyrics-container">
-          <button id="show-lyrics" class="show-lyrics">
-            <i class="fas fa-arrow-circle-up"></i>
-          </button>
-          <p class="lyrics-text" id="lyrics-text">${music.MusicLyricsText}</p>
-        </div>
-        <audio id="audio"  src=${music.musicSrc}></audio>
-      </div> `;
-  container.append(playingContainer);
-}
-
 const playerContainer = _id("player-container");
-// const container = _id("container");
-const showLyrics = _id("show-lyrics");
+const container = _id("container");
 const shuffle = _id("shuffle");
 const prev = _id("prev");
 const play = _id("play");
@@ -396,16 +47,148 @@ const next = _id("next");
 const repeat = _id("repeat");
 const favorite = _id("favorite");
 const moreMusic = _id("more-music");
+
 const audio = _id("audio");
 const lyricsText = _id("lyrics-text");
 const title = _id("title");
 const artist = _id("artist");
 const image = _id("image");
+const duration = _id("duration");
+const currentTime = _id("current-time");
+
+const volume = _id("volume");
+const track = _id("track");
+var canvas = _id("canvas");
 
 const show = _sel(".show-lyrics .fas");
 const lyricsContainer = _sel(".lyrics-container");
 const playBtn = _sel(".play .fas");
-audio.play();
-// play.addEventListener("click", () => {
-//   console.log("play");
-// });
+
+// Initialize Music
+let currentMusic = 0;
+// Initialize volume
+// audio.volume = 0;
+
+// Load Music**********************************
+loadMusic(currentMusic);
+function loadMusic(currentMusic) {
+  audio.src = musics[currentMusic].musicSrc;
+  image.src = musics[currentMusic].musicImageSrc;
+  title.innerHTML = musics[currentMusic].musicTitle;
+  artist.innerHTML = musics[currentMusic].musicAuthor;
+  playerContainer.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 1), rgba(0, 0, 0, 0.6)),url(${musics[currentMusic].musicImageSrc})`;
+}
+
+// Load Music*********************************X*
+
+// Event listeners****************************
+
+// shuffleBtn
+shuffle.addEventListener("click", () => {
+  console.log("shuffle");
+});
+
+// prevBtn
+prev.addEventListener("click", () => {
+  console.log("prev");
+  currentMusic--;
+  if (currentMusic < 0) {
+    currentMusic = musics.length - 1;
+  }
+  loadMusic(currentMusic);
+  audio.play();
+  if (playBtn.classList.contains("fa-play")) {
+    // audio.play();
+    playBtn.classList.remove("fa-play");
+    playBtn.classList.add("fa-pause");
+  }
+});
+
+// playBtn
+play.addEventListener("click", () => {
+  console.log("play");
+  if (playBtn.classList.contains("fa-play")) {
+    audio.play();
+    playBtn.classList.remove("fa-play");
+    playBtn.classList.add("fa-pause");
+  } else if (playBtn.classList.contains("fa-pause")) {
+    audio.pause();
+    playBtn.classList.remove("fa-pause");
+    playBtn.classList.add("fa-play");
+  }
+});
+
+// nextBtn
+next.addEventListener("click", () => {
+  console.log("next");
+  // Increasing the Number of the Initialize Music to next the Music.....
+  nextFunction();
+});
+
+// nextFunction
+function nextFunction() {
+  currentMusic++;
+  if (currentMusic > musics.length - 1) {
+    currentMusic = 0;
+  }
+  loadMusic(currentMusic);
+  audio.play();
+  if (playBtn.classList.contains("fa-play")) {
+    // audio.play();
+    playBtn.classList.remove("fa-play");
+    playBtn.classList.add("fa-pause");
+  }
+}
+
+// next misic when music Ended
+audio.addEventListener("ended", () => {
+  nextFunction();
+});
+
+// repeatBtn
+repeat.addEventListener("click", () => {
+  console.log("repeat");
+});
+
+// moreMusicBtn
+moreMusic.addEventListener("click", () => {
+  console.log("moreMusic");
+});
+
+// favoriteBtn
+favorite.addEventListener("click", () => {
+  console.log("favorite");
+});
+
+// favoriteBtn
+volume.addEventListener("mousemove", () => {
+  console.log(volume.value);
+  audio.volume = volume.value / 100;
+});
+
+// audio time update
+audio.addEventListener("timeupdate", () => {
+  updateSlider();
+});
+
+function updateSlider() {
+  const sliderUpdate = audio.currentTime * (100 / audio.duration);
+  track.value = sliderUpdate;
+  track.style.background = `linear-gradient(90deg, purple ${track.value}%, white ${track.value}%)`;
+
+  // DURATION
+  const minDuration = Math.floor(audio.duration / 60);
+  const secDuration = Math.floor(audio.duration - minDuration * 60);
+
+  // CURRENT TIME
+  const currentTimeMin = Math.floor(audio.currentTime / 60);
+  const currentTimeSec = Math.floor(audio.currentTime - currentTimeMin * 60);
+  console.log(audio.currentTime);
+  console.log(audio.currentTime - currentTimeMin * 60);
+  currentTime.innerHTML = currentTimeMin + ":" + currentTimeSec;
+  duration.innerHTML = minDuration + ":" + secDuration;
+}
+
+// Event listeners**************************X**
+
+// *X******** Music Analizer ***********X*
